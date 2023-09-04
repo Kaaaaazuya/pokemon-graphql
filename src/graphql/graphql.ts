a8d790c4dfe9fee9b4904eb7cb3fabc29ab18169
Kazuya Yokoyama <yk3kzy@gmail.com>
2023-09-04 09:15:14 +0900

    二重でリクエストが発生するため suspense を廃止

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -3,7 +3,6 @@ import { Client, cacheExchange, fetchExchange } from '@urql/core'
 
 export const client = new Client({
   url: 'https://beta.pokeapi.co/graphql/v1beta',
-  suspense: true,
   exchanges: [cacheExchange, fetchExchange],
   requestPolicy: 'cache-and-network',
 })
